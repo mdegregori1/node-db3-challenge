87b7abb584f375ac50cd155a1dcb8a52f713f2f4
Mauricio Degregori <degregori2@gmail.com>
2019-12-11 16:35:07 -0600

    sanity check passed

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.send(`<h2>Sanity TestDB3!</h2>`);
+  });
+
+
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
