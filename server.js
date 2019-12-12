8f82a64fd15ef8dcc4636956b90d5426aa06fd40
Mauricio Degregori <degregori2@gmail.com>
2019-12-11 19:31:13 -0600

    find function working

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const SchemeRouter = require('./schemes/scheme-router.js');
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send(`<h2>Sanity TestDB3!</h2>`);
+    res.send(`<h2>TestDB3!</h2>`);
   });
 
 
